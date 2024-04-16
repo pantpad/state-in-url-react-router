9c68c9f341c7d0ae14b12b284e57224585319ab0
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-16 18:50:05 +0200

    refactor(nav): add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100">
-        <div className="flex gap-4">
-          <a href="#">Home</a>
-          <a href="#">Store</a>
-        </div>
-      </main>
+      <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100"></main>
     </>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <>
+      <nav className="flex gap-4">
+        <a href="#">Home</a>
+        <a href="#">Store</a>
+      </nav>
+    </>
+  );
+}
