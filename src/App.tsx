a7eab52da6c5c783aac50f52517303b37fd32b85
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-16 18:53:20 +0200

    feat(form): add form to add items and filter

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ export default function App() {
     <>
       <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100">
         <Nav />
+        <section className="text-left">
+          <h1>Current Page</h1>
+        </section>
+
+        <ul></ul>
       </main>
     </>
   );
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+export default function Form() {
+  return (
+    <>
+      <form>
+        <label htmlFor="">Title</label>
+        <input type="text" />
+        <label htmlFor="">Only Computer Items</label>
+        <input type="checkbox" />
+      </form>
+    </>
+  );
+}
