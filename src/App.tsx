3f812bbaf82f91198e65c2aecaa1520620ecec81
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-16 18:54:20 +0200

    feat(currentpage): add current page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import CurrentPage from "./components/CurrentPage";
+import Form from "./components/Form";
 import Nav from "./components/Nav";
 
 export default function App() {
@@ -6,10 +8,8 @@ export default function App() {
     <>
       <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100">
         <Nav />
-        <section className="text-left">
-          <h1>Current Page</h1>
-        </section>
-
+        <CurrentPage />
+        <Form />
         <ul></ul>
       </main>
     </>
---
+++ src/components/CurrentPage.tsx
@@ -0,0 +1,7 @@
+export default function CurrentPage() {
+  return (
+    <section className="text-left">
+      <h1>Current Page</h1>
+    </section>
+  );
+}
