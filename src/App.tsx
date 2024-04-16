a4662c0ac35f8deeeab4da3834c1c594d71cc1f0
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-16 18:50:29 +0200

    chore(app): add nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <>
-      <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100"></main>
+      <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100">
+        <Nav />
+      </main>
     </>
   );
 }
