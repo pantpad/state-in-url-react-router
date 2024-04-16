c83eb3550a6edc839864612e400dfcf2574a4861
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-16 18:49:08 +0200

    style(app): style home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <div className="flex gap-4">
-        <a href="#">Home</a>
-        <a href="#">Store</a>
-      </div>
+      <main className="min-w-[min(32rem,100vw-2rem)] p-2 bg-red-100">
+        <div className="flex gap-4">
+          <a href="#">Home</a>
+          <a href="#">Store</a>
+        </div>
+      </main>
     </>
   );
 }
