57a11ae4d90c58818ef38cec3de1c3f5a2bb742a
pantpad <138708449+pantpad@users.noreply.github.com>
2024-04-21 17:33:05 +0200

    feat: Add Outlet to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
-import CurrentPage from "./components/CurrentPage";
+
 import Filters from "./components/Filters";
 import List from "./components/List";
 import Nav from "./components/Nav";
+
+import { Outlet } from "react-router-dom";
 
 import { mockList as allItems } from "./mockItems/list";
 
@@ -13,7 +15,9 @@ export default function App() {
     <>
       <main className="min-w-[min(32rem,100vw-2rem)] py-2 px-3 bg-red-100 flex flex-col gap-2 text-slate-600">
         <Nav />
-        <CurrentPage />
+        <div>
+          <Outlet />
+        </div>
         <Filters />
         <List list={allItems} />
       </main>
